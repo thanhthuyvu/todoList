c159fb59b76ae9948ba79049b55c78b9a96b8ea0
thanhthuyvu <44015483+thanhthuyvu@users.noreply.github.com>
2020-04-02 14:26:49 +0200

    Add database and create new custom list function

--- app.js
+++ app.js
@@ -1,38 +1,157 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-let todos =["Hoc Bai", "Mua Do"];
-
-app.post("/", function(req, res){
-let todo = req.body.newItem;
-todos.push(todo);
-res.redirect("/");
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 
+const itemsSchema = {
+  name: String
+};
+const Item = mongoose.model("Item", itemsSchema);
 
-app.get("/", function(req, res) {
-  let today = new Date();
-let options = {
-  weekday: "long",
-  day: "numeric",
-  month: "long"
+const item1 = new Item({
+  name: "Welcome to your todo List"
+});
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+const item3 = new Item({
+  name: " <-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
 };
 
-let day = today.toLocaleDateString("en-US",options);
+const List = mongoose.model("List", listSchema);
 
-  res.render("list", {
-    todos: todos ,
-    kindOfDay: day
+
+app.post("/", function(req, res) {
+
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+
+
+  const newItem = new Item({
+    name: itemName
   });
 
+  if (listName === "Today") {
+    newItem.save();
+    res.redirect("/");
+  } else {
+    List.findOne({
+      name: listName
+    }, function(err, foundList) {
+
+      foundList.items.push(newItem);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
+});
+
+app.post("/delete", function(req, res) {
+
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === "Today") {
+    Item.deleteOne({
+      _id: checkedItemId
+    }, function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect("/");
+      }
+    });
+  } else {
+    List.findOneAndUpdate({
+      name: listName
+    }, {
+      $pull:{items: {_id: checkedItemId}}
+    }, function(err, foundList) {
+      if(!err){
+        res.redirect("/"+listName);
+      }
+    });
+  }
+
+});
+
+
+app.get("/:customListName", function(req, res) {
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.findOne({
+    name: customListName
+  }, function(err, foundList) {
+    if (!err) {
+      if (!foundList) {
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        res.redirect("/" + customListName);
+      } else {
+        res.render("list", {
+          todos: foundList.items,
+          listTitle: foundList.name
+        });
+      }
+    }
+  });
+
+
+});
+
+app.get("/", function(req, res) {
+  // let today = new Date();
+  // let options = {
+  //   weekday: "long",
+  //   day: "numeric",
+  //   month: "long"
+  // };
+  //
+  // let day = today.toLocaleDateString("en-US", options);
+
+  Item.find({}, function(err, foundItems) {
+
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Items have been added to Database!");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", {
+        todos: foundItems,
+        listTitle: "Today"
+      });
+    };
+  });
 });
 
 
